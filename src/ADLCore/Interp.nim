cf61c5f70b6c51924a8d63871366f5101bf307f2
ShujianDou <visionistprojects@gmail.com>
2022-08-23 19:26:16 -0400

    export code; problems with exporting function for call

--- src/ADLCore/Interp.nim
+++ src/ADLCore/Interp.nim
@@ -56,24 +56,6 @@ proc ParseInfoTuple(file: string): InfoTuple =
     else: break
   return infoTuple
 
-proc GetHTMLNode(node: XmlNode, path: varargs[tuple[key: string, attrs: seq[tuple[k, v: string]]]]): XmlNode =
-  var currentNode = node
-  for key in path:
-    var i: int = 0
-    for node in currentNode.items:
-      var aChk: seq[bool] = @[]
-      if node.kind != xnElement: continue
-      if node.tag != key.key: continue
-      if key.attrs.len > 0 and key.attrs[0].k == "nth" and parseInt(key.attrs[0].v) < i:
-        inc i
-        continue
-      for attr in key.attrs:
-        if node.attr(attr.k) == attr.v: aChk.add true
-      if len(aChk) != len(key.attrs): continue
-      currentNode = node
-      break
-  return currentNode
-
 proc ReadScriptInfoTuple*(path: string): InfoTuple =
   var infoTuple = ParseInfoTuple(readFile(path))
   infoTuple.scriptPath = path
@@ -86,7 +68,25 @@ proc processHttpRequest(uri: string, scriptID: int, headers: seq[tuple[key: stri
   return NScriptClient[scriptID].getContent(uri)
 
 exportTo(ADLNovel, InfoTuple, Status, LanguageType, MetaData,
-  Image, TiNode, processHttpRequest, GetHTMLNode)
+  Image, TiNode, processHttpRequest)
+exportCode(ADLNovel):
+  proc GetHTMLNode*(node: XmlNode, path: varargs[tuple[key: string, attrs: seq[tuple[k, v: string]]]]): XmlNode =
+    var currentNode = node
+    for key in path:
+      var i: int = 0
+      for node in currentNode.items:
+        var aChk: seq[bool] = @[]
+        if node.kind != xnElement: continue
+        if node.tag != key.key: continue
+        if key.attrs.len > 0 and key.attrs[0].k == "nth" and parseInt(key.attrs[0].v) < i:
+          inc i
+          continue
+        for attr in key.attrs:
+          if node.attr(attr.k) == attr.v: aChk.add true
+        if len(aChk) != len(key.attrs): continue
+        currentNode = node
+        break
+    return currentNode
 const novelInclude = implNimScriptModule(ADLNovel)
 
 
